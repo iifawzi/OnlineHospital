ca166bf68717be45cc2a21adaa8112d7e3960f18
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-05-02 21:12:04 +0200

    edit routes file

--- startup/routes.js
+++ startup/routes.js
@@ -3,13 +3,10 @@ const { handleError, ErrorHandler } = require("../middleware/error"); // error m
 const mainRouter = require("../routes/mainRoutes");
 
 module.exports = (app) => {
-  // for testing
-  app.get("/", (req, res, next) => {
-    res.send("<h1> Hello World!</h1>");
-  });
-
+  
   // Routers:
   app.use("/api/", mainRouter);
+
   // Error handling
   app.use((err, req, res, next) => {
     handleError(err, res);
