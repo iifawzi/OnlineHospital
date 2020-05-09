353a5b10e421e2ad42d50593459df0e66ecba41f
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-05-09 09:36:08 +0200

    re-design the users file

--- models/users.js
+++ models/users.js
@@ -1,113 +1,116 @@
 const Sequelize = require("sequelize");
 const db = require("../utils/db");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-const users = db.define('users',{
-phone_number: {
-    type: Sequelize.STRING(255),
-    allowNull: false,
-},
-username: {
-    type: Sequelize.STRING(255),
-    allowNull: false,
-},
-first_name: {
-    type: Sequelize.STRING(255),
-    allowNull: false,
-},
-last_name: {
-    type: Sequelize.STRING(255),
-    allowNull: false,
-},
-password: {
-    type: Sequelize.STRING(255),
-    allowNull: false,
-},
-province: {
-    type: Sequelize.STRING(255),
-    allowNull: false,
-},
-is_activited: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-    defaultValue: 0,
-},
-role: {
-    type: Sequelize.DataTypes.ENUM('user', 'admin','doctor'),
-    allowNull: false,
-    defaultValue: "user",
-},
-},{
+const users = db.define(
+  "users",
+  {
+    phone_number: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    username: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    first_name: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    last_name: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    password: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    province: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    is_activited: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    role: {
+      type: Sequelize.DataTypes.ENUM("user", "admin", "doctor"),
+      allowNull: false,
+      defaultValue: "user",
+    },
+  },
+  {
     freezeTableName: true,
-    timestamps: false
-})
+    timestamps: false,
+  }
+);
 
-// Methods: 
-//     --- CRUD -- 
-        // Creating new user
-const createUser = async function(body){
-    const user = await users.create({...body});
-    return user;
-    }
-        // DELETE USER BY USERNAME
-        const deletedUser = async function(username){
-            const user = await users.destroy({where:{username}});
-            return user;
-            }
-//     --- END CRUD -- 
+// Methods:
+//     --- CRUD --
+// Creating new user
+const createUser = async function (body) {
+  const user = await users.create({ ...body });
+  return user;
+};
+// DELETE USER BY USERNAME
+const deletedUser = async function (username) {
+  const user = await users.destroy({ where: { username } });
+  return user;
+};
+//     --- END CRUD --
 
-
-// Check if user exists: 
-const checkIfUserExist = async function(username){
-        const user = await users.findOne({where:{username}});
-        return user;
+// Check if user exists:
+const checkIfUserExist = async function (username) {
+  const user = await users.findOne({ where: { username } });
+  return user;
 };
 
 // Check if phone number exits:
-const checkIfPhoneExist = async function(phone_number){
-const user = await users.findOne({where:{phone_number}});
-return user;
+const checkIfPhoneExist = async function (phone_number) {
+  const user = await users.findOne({ where: { phone_number } });
+  return user;
 };
 
 // Hashing the password
-const hashPassword = async function(password){
-       const hashed = await bcrypt.hash(password, config.get("bcrypt.saltRounds"));
-       return hashed;
-}
+const hashPassword = async function (password) {
+  const hashed = await bcrypt.hash(password, config.get("bcrypt.saltRounds"));
+  return hashed;
+};
 
-// Verify the password 
-const verifyPassword = async function(password,dbPass){
-    return  bcrypt.compare(password,dbPass);
-}
+// Verify the password
+const verifyPassword = async function (password, dbPass) {
+  return bcrypt.compare(password, dbPass);
+};
 
-// Generate token: 
-const genToken = function(username,userRole){
-    const encData = {
-        username,
-        userRole,
-    };
+// Generate token:
+const genToken = function (username, userRole) {
+  const encData = {
+    username,
+    userRole,
+  };
 
-     // data to be encrypted in the JSONWEBTOKEN.
-    return jwt.sign(encData,config.get("jwt.secret"),{
-    expiresIn: config.get("jwt.expiresIn")
-    });
-}
+// data to be encrypted in the JSONWEBTOKEN.
+  return jwt.sign(encData, config.get("jwt.secret"), {
+    expiresIn: config.get("jwt.expiresIn"),
+  });
+};
 
-const signinCheck = function(username,password){
-  const user =  users.findOne({where:{username}});
-return user;
+const signinCheck = function (username, password) {
+  const user = users.findOne({ where: { username } });
+  return user;
 };
 
 module.exports = {
-    users,
-    checkIfUserExist,
-    checkIfPhoneExist,
-    createUser,
-    hashPassword,
-    verifyPassword,
-    genToken,
-    signinCheck,
-    deletedUser,
+  users,
+  checkIfUserExist,
+  checkIfPhoneExist,
+  createUser,
+  hashPassword,
+  verifyPassword,
+  genToken,
+  signinCheck,
+  deletedUser,
 };
