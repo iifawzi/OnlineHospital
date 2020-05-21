41398f143093027bfb298fdf4240fab459e49b50
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-05-22 01:11:51 +0200

    make pictures avaliable

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const http = require("http");
 const app = express();
+const path = require("path");
 const port = process.env.PORT || 5000;
 
 require("./startup/models"); // indlude the db connection and models
 require("./startup/routes")(app); // include routes
-
+app.use('/images', express.static('uploadedImages'))
 const server = http.createServer(app);
 const init = server.listen(port, () => {
   console.log(`server is running at ${port}`);
