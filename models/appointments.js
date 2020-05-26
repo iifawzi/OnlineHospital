49bd55cda8fa297ef62ec86efb9b745f8288a13f
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-05-26 20:19:04 +0200

    edit appointments model

--- models/appointments.js
+++ models/appointments.js
@@ -13,19 +13,39 @@ appointment_id : {
 slot_id: {
     type: Sequelize.INTEGER,
     allowNull: false,
+    references: {    
+        model: 'slots',
+        key: 'slot_id'
+      },
 },
 user_id:{
     type:Sequelize.INTEGER,
     allowNull: false,
+    references: {    
+        model: 'users',
+        key: 'user_id'
+      },
 },
 date: {
     type:Sequelize.DATE,
     allowNull:false,
 },
 appointment_status: {
-    type: Sequelize.ENUM("upcoming","running","finished","canceled"),
+    type: Sequelize.ENUM("pending","upcoming","running","finished","canceled"),
     allowNull: false,
 }
+},{
+    indexes: [
+        {
+            fields: ["slot_id","date","appointment_status"],
+          },
+          {
+            fields: ["user_id","date"],
+          },
+      ],
+},{
+    freezeTableName: true,
+    timestamps: false,
 })
 
 
