e754e7b61c115310f6ab9d0a0a0f42aeb6dd42e7
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-06-02 14:03:03 +0200

    edit the slots routes

--- routes/slots.js
+++ routes/slots.js
@@ -8,9 +8,8 @@ const slotsController = require("../controllers/slots");
 
 router.post("/addSlot",isAuth(["admin"]),validate(validationSchemas.addSlot,"body"),slotsController.addDocSlot);
 router.post("/getDoctorDays",isAuth(["user","doctor",'admin']),validate(validationSchemas.getDoctorDays,"body"),slotsController.getDoctorDays);
-router.post("/getOpenSlots",isAuth(["admin","user"]),validate(validationSchemas.getOpenSlots,"body"),slotsController.getOpenSlots);
-// TODO::later just ALLOW FOR USERS
-router.patch("/updateSlot",isAuth(["admin","user"]),validate(validationSchemas.updateSlot,"body"),slotsController.updateSlot);
+router.post("/getOpenSlots",isAuth(["user"]),validate(validationSchemas.getOpenSlots,"body"),slotsController.getOpenSlots);
+router.patch("/updateSlot",isAuth(["admin"]),validate(validationSchemas.updateSlot,"body"),slotsController.updateSlot);
 
 
 module.exports = router;
