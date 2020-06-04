93e8b3b8f361718b0aeb1753a789629a20f100b2
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-06-04 12:24:32 +0200

    update expires in

--- utils/shared/genToken.js
+++ utils/shared/genToken.js
@@ -7,9 +7,17 @@ const genToken = function (phone_number,id, role) {
       id
     };
     // data to be encrypted in the JSONWEBTOKEN.
-    return jwt.sign(encData, config.get("jwt.secret"), {
-      expiresIn: config.get("jwt.expiresInAdmins"),
-    });
+    let token = "";
+    if (role === "admin" || role === "moderator"){
+      token =  jwt.sign(encData, config.get("jwt.secret"), {
+        expiresIn: config.get("jwt.expiresInAdmins"),
+      });
+    }else {
+      token =  jwt.sign(encData, config.get("jwt.secret"), {
+        expiresIn: config.get("jwt.expiresIn"),
+      });
+    }
+    return token;
   };
 
   module.exports = {
