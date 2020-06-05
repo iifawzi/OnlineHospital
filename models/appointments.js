a10943a8a0949577d84d822e7dd2bbb5089818f0
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-06-05 18:03:23 +0200

    update responses

--- models/appointments.js
+++ models/appointments.js
@@ -120,7 +120,7 @@ const userApps = async function(user_id,res){
 
 const upcomingApps = async function(user_id,res){
     try {
-        const appointments =  await db.query("SELECT apps.date, apps.appointment_id,docs.first_name,docs.last_name,apps.appointment_status,CONCAT(apps.date,'T',slots.start_time,'Z') start_time,slots.slot_time,cats.ar,cats.en, slots.day FROM appointments apps INNER JOIN slots ON apps.slot_id = slots.slot_id INNER JOIN doctors docs ON slots.doctor_id = docs.doctor_id INNER JOIN categories cats ON cats.category_id = docs.category_id WHERE apps.user_id = ? AND (apps.appointment_status = 'upcoming' || apps.appointment_status = 'running') ORDER BY apps.appointment_id DESC",{
+        const appointments =  await db.query("SELECT apps.appointment_id,docs.first_name,docs.last_name,apps.appointment_status,CONCAT(apps.date,'T',slots.start_time,'Z') start_time,slots.slot_time,cats.ar,cats.en, slots.day FROM appointments apps INNER JOIN slots ON apps.slot_id = slots.slot_id INNER JOIN doctors docs ON slots.doctor_id = docs.doctor_id INNER JOIN categories cats ON cats.category_id = docs.category_id WHERE apps.user_id = ? AND (apps.appointment_status = 'upcoming' || apps.appointment_status = 'running') ORDER BY apps.appointment_id DESC",{
             replacements: [user_id],
             type: Sequelize.QueryTypes.SELECT,
         });
@@ -144,7 +144,7 @@ const finishedApps = async function(user_id,res){
 }
 const doctorUpcomingApps = async function(doctor_id,res){
     try {
-        const appointments =  await db.query("SELECT apps.date, apps.appointment_id,users.first_name,users.last_name,apps.appointment_status,CONCAT(apps.date,'T',slots.start_time,'Z') start_time,slots.slot_time, slots.day FROM appointments apps INNER JOIN slots ON apps.slot_id = slots.slot_id INNER JOIN users ON users.user_id = apps.user_id INNER JOIN doctors docs ON slots.doctor_id = docs.doctor_id INNER JOIN categories cats ON cats.category_id = docs.category_id WHERE slots.doctor_id = ? AND (apps.appointment_status = 'upcoming' || apps.appointment_status = 'running') ORDER BY apps.appointment_id DESC",{
+        const appointments =  await db.query("SELECT apps.appointment_id,users.first_name,users.last_name,apps.appointment_status,CONCAT(apps.date,'T',slots.start_time,'Z') start_time,slots.slot_time, slots.day FROM appointments apps INNER JOIN slots ON apps.slot_id = slots.slot_id INNER JOIN users ON users.user_id = apps.user_id INNER JOIN doctors docs ON slots.doctor_id = docs.doctor_id INNER JOIN categories cats ON cats.category_id = docs.category_id WHERE slots.doctor_id = ? AND (apps.appointment_status = 'upcoming' || apps.appointment_status = 'running') ORDER BY apps.appointment_id DESC",{
             replacements: [doctor_id],
             type: Sequelize.QueryTypes.SELECT,
         });
