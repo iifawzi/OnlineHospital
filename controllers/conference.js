d1b45cd29a1074040f73a41d98db13a359e77453
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-06-11 23:33:19 +0200

    update response

--- controllers/conference.js
+++ controllers/conference.js
@@ -5,21 +5,21 @@ const {getStartTimeByRoom} = require("../models/appointments");
 
 const checkConference = async (req, res, next) => {
   try {
-    const room_id = req.body.name;
-    const appointment = await getStartTimeByRoom(room_id, res);
-    const { start_time, slot_time } = appointment;
-    let end_time = moment(start_time)
+    const {name,start_time,mail_owner} = req.body;
+    const appointment = await getStartTimeByRoom(name, res);
+    const {slot_time } = appointment;
+    let end_time = moment(appointment.start_time)
       .add(slot_time, "m")
       .format("YYYY-MM-DD HH:mm:ss");
     let currentTime = moment().utc();
     let finish_time = moment(end_time).utc();
     let remainingTime = finish_time.diff(currentTime,'seconds');
     const room_info = {
-        'id': room_id,
-        'name': room_id,
-        'start_time': moment().format(),
+        'id': name,
+        'name': name,
+        'start_time': start_time,
         'duration': remainingTime,
-        'mail_owner': 'user@server.com',
+        'mail_owner': mail_owner,
     }
     console.log(room_info);
    return res.status(200).json(room_info);
