f934de5b98c706254184128dd2a242d702f3d697
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-06-12 00:25:22 +0200

    update time

--- controllers/conference.js
+++ controllers/conference.js
@@ -18,7 +18,7 @@ const checkConference = async (req, res, next) => {
     const room_info = {
         id: 837387,
         name: name,
-        start_time: start_time,
+        start_time: appointment.start_time,
         duration: remainingTime,
     }
     console.log(room_info);
